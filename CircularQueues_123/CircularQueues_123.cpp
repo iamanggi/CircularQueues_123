ce2acd19312e6b3503c5906acd7a9111d72f67cd
iamanggi <anggipuspita452@gmail.com>
2023-06-22 15:01:24 +0700

    membuat pengecekan antrian apakah sudah penuh atau belum

--- CircularQueues_123/CircularQueues_123.cpp
+++ CircularQueues_123/CircularQueues_123.cpp
@@ -15,5 +15,11 @@ public:
 		cout << "Enter a Number : ";
 		cin >> num;
 		cout << endl;
+
+		// cek apakah antrian penuh 
+		if ((FRONT == 0 && REAR == Max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
 	}
 }
